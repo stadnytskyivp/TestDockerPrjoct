4ad724a4086b2df58752cf0835c8cc2e93c7924b
stadnytskyivp <stadnytskyi.volodymyr@gmail.com>
2020-09-11 16:18:00 +0300

    first push project

---
+++ src/test/java/myTest/BasicTest.java
@@ -0,0 +1,14 @@
+package myTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BasicTest {
+
+    @Test
+    public void simpleTest() {
+        System.out.println("=)");
+//        Assert.assertEquals(1,1);
+    }
+
+}
